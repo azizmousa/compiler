4a698553a0e31a749efeccb118717a41b1be8a40
Aziz Mousa <azizmousa1010@gmail.com>
2019-04-14 23:48:51 +0200

    app settings Loader and controller

---
+++ include/compiler/Loader.h
@@ -0,0 +1,34 @@
+#ifndef NETWORKS_LOADER_
+#define NETWORKS_LOADER_
+
+#include <string>
+#include <vector>
+#include <opencv2/dnn/dnn.hpp>
+
+class Loader{
+private:
+    std::string weightsPath, namesPath, configurationsPath;
+
+    std::vector<std::string> weightsVector, namesVector, cfgVector;
+    std::vector<cv::dnn::Net> networks;
+    std::vector<std::vector<std::string>> classes;
+    
+    void loadVectors();
+    void loadNets();
+    void loadClasses();
+
+public:
+
+    Loader(const std::string weightsPath, const std::string namesPath, const std::string configurationsPath);
+    ~Loader();
+
+    size_t getNetworksNumber()const;
+    size_t getPublicClassesNumber()const;
+    
+    cv::dnn::Net getNet(const size_t index)const;
+    void getClassesVector(const size_t index, std::vector<std::string> &classes)const;
+    std::string getClass(const size_t vIndex, const size_t cIndex)const;
+
+};
+
+#endif
---
+++ src/Loader.cpp
@@ -0,0 +1,64 @@
+
+#include "compiler/Loader.h"
+#include "compiler/Files.h"
+
+#include <fstream>
+
+Loader::Loader(const std::string weightsPath, const std::string namesPath, const std::string configurationsPath){
+    this->weightsPath = weightsPath;
+    this->namesPath = namesPath;
+    this->configurationsPath = configurationsPath;
+
+    this->loadVectors();
+    this->loadClasses();
+    this->loadNets();    
+}
+
+void Loader::loadVectors(){
+    Files::getDirFiles(this->weightsPath, ".weights", this->weightsVector);
+    Files::getDirFiles(this->namesPath, ".names", this->namesVector);
+    Files::getDirFiles(this->configurationsPath, ".cfg", this->cfgVector);
+}
+void Loader::loadNets(){
+    for(size_t i =0; i < this->weightsVector.size(); ++i){
+        // Give the configuration and weight files for the model
+        std::string modelConfiguration = this->configurationsPath + "/" + cfgVector[i];
+        std::string modelWeights = this->weightsPath + "/" +weightsVector[i];
+        // std::cerr << modelConfiguration << "\t" << modelWeights << std::endl;
+        // Load the network
+        cv::dnn::Net net = cv::dnn::readNetFromDarknet(modelConfiguration, modelWeights);
+        net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+        net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+
+        this->networks.push_back(net);
+     }
+}
+void Loader::loadClasses(){
+    for(size_t i = 0; i < this->weightsVector.size();++i){
+        // Load names of classes
+        std::string classesFile = this->namesPath + "/" +namesVector[i];
+        std::ifstream ifs(classesFile.c_str());
+        std::string line;
+        std::vector<std::string> temp;
+        while (getline(ifs, line)) temp.push_back(line);
+
+        this->classes.push_back(temp);
+    }
+}
+
+size_t Loader::getNetworksNumber()const{
+    return this->networks.size();
+}
+size_t Loader::getPublicClassesNumber()const{
+    return this->classes.size();
+}
+
+cv::dnn::Net Loader::getNet(const size_t index)const{
+    return this->networks[index];
+}
+void Loader::getClassesVector(const size_t index, std::vector<std::string> &classes)const{
+    classes = this->classes[index];
+}
+std::string Loader::getClass(const size_t vIndex, const size_t cIndex)const{
+    return this->classes[vIndex][cIndex];
+}
